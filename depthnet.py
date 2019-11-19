2153e17cabbcd0a1748b8c1347facedb2e0d9f78
Owen Thompson <oxt3479@cad-cuda-2191.rit.edu>
2019-11-19 16:02:54 -0500

    First attempt at 3 image network input.

--- depthnet.py
+++ depthnet.py
@@ -348,22 +348,22 @@ class ResnetModel(nn.Module):
         self.encoder4 = resnet.layer4 # H/32
 
         # decoder
-        self.upconv6 = upconv(filters[3], 512, 3, 2)
-        self.iconv6 = conv(filters[2] + 512, 512, 3, 1)
+        self.upconv6 = upconv(filters[3]*3, 512, 3, 2)
+        self.iconv6 = conv(filters[2]*3 + 512, 512, 3, 1)
 
         self.upconv5 = upconv(512, 256, 3, 2)
-        self.iconv5 = conv(filters[1] + 256, 256, 3, 1)
+        self.iconv5 = conv(filters[1]*3 + 256, 256, 3, 1)
 
         self.upconv4 = upconv(256, 128, 3, 2)
-        self.iconv4 = conv(filters[0] + 128, 128, 3, 1)
+        self.iconv4 = conv(filters[0]*3 + 128, 128, 3, 1)
         self.disp4_layer = get_disp(128)
 
         self.upconv3 = upconv(128, 64, 3, 1) #
-        self.iconv3 = conv(64 + 64 + 2, 64, 3, 1)
+        self.iconv3 = conv(128 + 128 + 2, 64, 3, 1)
         self.disp3_layer = get_disp(64)
 
         self.upconv2 = upconv(64, 32, 3, 2)
-        self.iconv2 = conv(64 + 32 + 2, 32, 3, 1)
+        self.iconv2 = conv(226, 32, 3, 1)
         self.disp2_layer = get_disp(32)
 
         self.upconv1 = upconv(32, 16, 3, 2)
@@ -375,46 +375,71 @@ class ResnetModel(nn.Module):
                 nn.init.xavier_uniform_(m.weight)
 
     def forward(self, x):
-        # encoder
-        x_first_conv = self.firstconv(x)
-        x = self.firstbn(x_first_conv)
-        x = self.firstrelu(x)
-        x_pool1 = self.firstmaxpool(x)
-        x1 = self.encoder1(x_pool1)
-        x2 = self.encoder2(x1)
-        x3 = self.encoder3(x2)
-        x4 = self.encoder4(x3)
+        # x = [img1, img2, img3]
+        
+        x4 = []
+
+        # encoder n-1
+        x_first_convnM = self.firstconv(x[0])
+        x[0] = self.firstbn(x_first_convnM)
+        x[0] = self.firstrelu(x[0])
+        x_pool1nM = self.firstmaxpool(x[0])
+        x1nM = self.encoder1(x_pool1nM)
+        x2nM = self.encoder2(x1nM)
+        x3nM = self.encoder3(x2nM)
+        x4.append(self.encoder4(x3nM))
+
+        # encoder n
+        x_first_convn = self.firstconv(x[1])
+        x[1] = self.firstbn(x_first_convn)
+        x[1] = self.firstrelu(x[1])
+        x_pool1n = self.firstmaxpool(x[1])
+        x1n = self.encoder1(x_pool1n)
+        x2n = self.encoder2(x1n)
+        x3n = self.encoder3(x2n)
+        x4.append(self.encoder4(x3n))
+
+        # encoder n+1
+        x_first_convnP = self.firstconv(x[2])
+        x[2] = self.firstbn(x_first_convnP)
+        x[2] = self.firstrelu(x[2])
+        x_pool1nP = self.firstmaxpool(x[2])
+        x1nP = self.encoder1(x_pool1nP)
+        x2nP = self.encoder2(x1nP)
+        x3nP = self.encoder3(x2nP)
+        x4.append(self.encoder4(x3nP))
+
         # skips
-        skip1 = x_first_conv
-        skip2 = x_pool1
-        skip3 = x1
-        skip4 = x2
-        skip5 = x3
+        skip1 = [x_first_convnM, x_first_convn, x_first_convnP,]
+        skip2 = [x_pool1nM, x_pool1n, x_pool1nP]
+        skip3 = [x1nM, x1n, x1nP]
+        skip4 = [x2nM, x2n, x2nP]
+        skip5 = [x3nM, x3n, x3nP]
 
         # decoder
-        upconv6 = self.upconv6(x4)
-        concat6 = torch.cat((upconv6, skip5), 1)
+        upconv6 = self.upconv6(torch.cat(x4,1))
+        concat6 = torch.cat((upconv6, torch.cat(skip5,1)), 1)
         iconv6 = self.iconv6(concat6)
 
         upconv5 = self.upconv5(iconv6)
-        concat5 = torch.cat((upconv5, skip4), 1)
+        concat5 = torch.cat((upconv5, torch.cat(skip4,1)), 1)
         iconv5 = self.iconv5(concat5)
 
         upconv4 = self.upconv4(iconv5)
-        concat4 = torch.cat((upconv4, skip3), 1)
+        concat4 = torch.cat((upconv4, torch.cat(skip3,1)), 1)
         iconv4 = self.iconv4(concat4)
         self.disp4 = self.disp4_layer(iconv4)
         self.udisp4 = nn.functional.interpolate(self.disp4, scale_factor=1, mode='bilinear', align_corners=True)
         self.disp4 = nn.functional.interpolate(self.disp4, scale_factor=0.5, mode='bilinear', align_corners=True)
 
         upconv3 = self.upconv3(iconv4)
-        concat3 = torch.cat((upconv3, skip2, self.udisp4), 1)
+        concat3 = torch.cat((upconv3, torch.cat(skip2,1), self.udisp4), 1)
         iconv3 = self.iconv3(concat3)
         self.disp3 = self.disp3_layer(iconv3)
         self.udisp3 = nn.functional.interpolate(self.disp3, scale_factor=2, mode='bilinear', align_corners=True)
 
         upconv2 = self.upconv2(iconv3)
-        concat2 = torch.cat((upconv2, skip1, self.udisp3), 1)
+        concat2 = torch.cat((upconv2, torch.cat(skip1,1), self.udisp3), 1)
         iconv2 = self.iconv2(concat2)
         self.disp2 = self.disp2_layer(iconv2)
         self.udisp2 = nn.functional.interpolate(self.disp2, scale_factor=2, mode='bilinear', align_corners=True)
