fbc7da8572c1c56f59fac932488e46516839266e
Owen Thompson <oxt3479@cad-cuda-2191.rit.edu>
2019-12-21 12:04:50 -0500

    Semester end commit: current implementation does not converge.

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ Student project WIP
 
 Building network to learn depth from stereo video movies
 
-Dataset consists of frames of stereo movies.
-
+Dataset consists of frames of stereo movies. The dataset is not included here due to concerns with copywrite. They will function with this implementation if placed as numpy arrays of stereo pairs in the numpy_img directory.
--- frames2nparray.py
+++ frames2nparray.py
@@ -11,23 +11,19 @@ class LeftToRight():
     IMG_SIZE = (1280,720) # Size of the movie frames
     LEFT = "left/"
     RIGHT = "right/"
-
-    MOVIES = os.listdir(LEFT)
-    MOVIES.remove('.ipynb_checkpoints')
     training_data = []
     size = 640, 256 # Size of NP arrays
     box = (0,100,1280,620) # Cropping of movie frames.
-    def make_training_data(self):
+    def make_training_data(self, movie_details):
         LEFT = self.LEFT
         RIGHT = self.RIGHT
         size = self.size
-        #for movie in self.MOVIES:
-        movie = self.MOVIES[-1]
+        movie = movie_details[0]
         box = self.box
         # First build left 3 img dataset.
         k = 0
         j = 0
-        for i in tqdm(range(1250, 128000)):
+        for i in tqdm(range(movie_details[1], movie_details[2])):
             try:            
                 #Reads in files from directories$ holding 720p frames of left and right stereo images
                 path_L = os.path.join(LEFT+movie, os.listdir(LEFT+movie)[i])
@@ -39,13 +35,28 @@ class LeftToRight():
                 print(str(e))
                 pass
             j+=1
-            if j == 10000 or i == 156000:
+            if j == 5000 or i == movie_details[2]:
                 k+=1
                 np.save(f"{movie}_lowres-{k}.npy", self.training_data)
                 self.training_data.clear()
                 j = 0
         
 if REBUILD_DATA:
+    frame_ends = [['hobbit2p2', 0, 109000]]
+
+
     lefttoright = LeftToRight()
-    lefttoright.make_training_data()
+    for i in frame_ends:
+        lefttoright.make_training_data(i)
+
+
+    frame_ends = [['hobbit2', 1500, 108150],
+        ['hobbit2p2', 0, 109000],
+        ['hobbit3', 1500, 118350],
+        ['hobbit3p2', 0, 70900],
+        ['residentevilafterlife', 900, 129375],
+        ['residentevilretribution', 1420, 123150],
+        ['transformer_knight', 0, 210700],
+        ['transformersmoon', 0, 210900],
+        ['xmen_apocalypse', 1400, 193000]]
 
